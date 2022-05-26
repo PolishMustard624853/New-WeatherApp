20b1e75c36d590f9833d611f21a339bdb0240a95
Connor Semb <polishmustard710@gmail.com>
2022-05-26 11:06:18 -0500

    Added API key and URL

---
+++ script.js
@@ -0,0 +1,11 @@
+const api = {
+    key: 'eb7c6c94cbc9aa24e851279707916e63',
+    url: 'api.openweathermap.org/data/2.5/',
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    
+}
