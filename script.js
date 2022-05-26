5ddabe03072dbb169a4a7d3a62f7ceee6ef4d701
Connor Semb <polishmustard710@gmail.com>
2022-05-26 13:55:13 -0500

    was having trouble fetching, solved the issue and added get results, display results, and datebuilder

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const api = {
-    key: 'eb7c6c94cbc9aa24e851279707916e63',
-    url: 'api.openweathermap.org/data/2.5/',
+    key: '2198eb51fd8e2b16429dd7ea2c6e4063',
+    base: 'https://api.openweathermap.org/data/2.5/forecast?'
 }
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
-// if keyCode(13) is registered, get the results
-function setQuery(event) {
-    if (event.keyCode == 13) {
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
         getResults(searchbox.value);
-        console.log(searchbox.value);
     }
 }
 
-function displayResults (weather) {
-    let city = 
+function getResults (query) {
+    fetch(`${api.base}q=${query}&units=metric&appid=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
 }
 
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+}
+
+function dateBuilder (d) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day =days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
